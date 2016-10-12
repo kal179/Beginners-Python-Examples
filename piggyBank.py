0c97b9c2f62e8f1aef71d93ef7c814043dd734d1
Kalpak Take <kalpaktake@gmail.com>
2016-10-12 22:08:31 +0530

    Update piggyBank.py

--- piggyBank.py
+++ piggyBank.py
@@ -4,24 +4,30 @@ money = 0
 
 # function to add money to current amount
 def addMoney():
+    print(" ")
     userAdd = float(raw_input("Add money : "))
+    print(" ")
     money = money + userAdd
     print("After adding current Money you have is " + str(money) + " rupees")
 
 # function to withdraw money from current amount
 def withdrawMoney():
+    print(" ")
     userWithdraw = float(raw_input("Add money : "))
+    print(" ")
     money = money + userWithdraw
     print("After adding current Money you have is " + str(money) + " rupees")
 
 # function to display current amount
 def currentMoney():
+    print(" ")
     current = "Current money you have is " + str(money) + " rupees"
 
 # main code
 print(" ")
 print("--------------------Start-------------------")
 while True:
+    print(" ")
     user = raw_input("Start or End : ")
     if user.strip() == "Start":
         controlPiggy = raw_input("Add Withdraw or Check : ")
@@ -35,6 +41,7 @@ while True:
             print(currentMoney())
             continue
         else :
+            print(" ")
             print("Invalid Input.Try again")
             continue
 
