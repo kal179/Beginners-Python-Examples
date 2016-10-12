478395afcda1feb4e05ff157d89a96081c5ae491
Kalpak Take <kalpaktake@gmail.com>
2016-10-12 10:38:54 +0530

    Update another calculator.py

--- another calculator.py
+++ another calculator.py
@@ -1,40 +1,43 @@
 
+
 def adda():
     spam=raw_input("enter number here:")
-	eggs=raw_input("enter next number :")
-	print(int(spam)+int(eggs))	
+    eggs=raw_input("enter next number :")
+    print(int(spam)+int(eggs))	
 	
 def extract():
     spam=int(raw_input("enter number here:"))
-	eggs=int(raw_input("enter next number :"))
-	print(spam-eggs)	
+    eggs=int(raw_input("enter next number :"))
+    print(spam-eggs)	
 	
 def multiple():
     spam=int(raw_input("enter number here:"))
-	eggs=int(raw_input("enter next number :"))
-	print(spam*eggs)	
+    eggs=int(raw_input("enter next number :"))
+    print(spam*eggs)	
 
 def divi():
-    spam=int(raw_input("enter number here:"))
-	eggs=int(raw_input("enter next number :"))
-	print(spam/eggs)
+    spam = int(raw_input("enter number here:"))
+    eggs = int(raw_input("enter next number :"))
+    try:
+       print(spam/eggs)
+    except ZeroDivisionError:
+        print("Cannot divide by Zero.")   
  
 def modula():
     spam=int(raw_input("enter number here:"))
-	eggs=int(raw_input("enter next number :"))
-	print(spam%eggs)
+    eggs=int(raw_input("enter next number :"))
+    print(spam%eggs)
 
-def calculas():
-    multiverse=raw_input("enter add,subtract,multiply,divide,modulo :")	
-	if multiverse==add:
-    adda()
-    elif multiverse==subtract:	
-	extract()
-	elif muliverse==multiply:
-    multiple()
-	elif multiverse==divide:
-	divi()
-	elif multiverse==modulo:
-	modula()
+user = raw_input("enter add,subtract,multiply,divide,modulo :")	
+if user == "add":
+    print(adda())
+elif user == "subtract":	
+    print(extract())
+elif user == "multiply":
+    print(multiple())
+elif user == "divide":
+    print(divi())
+elif user == "modulo":
+    print(modula())
 	
- calculas()
+
