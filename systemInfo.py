21f1bdefe929c058cbbc4d4af6df58dd4b19c914
Kalpak Take <kalpaktake@gmail.com>
2016-10-21 23:46:47 +0530

    Add files via upload

---
+++ systemInfo.py
@@ -0,0 +1,13 @@
+import platform  
+import os  
+import sys
+
+print('============================')
+print('System Information : ')
+print(os.name)  
+print(platform.system())  
+print(platform.release(),'\n')
+print('============================')
+print('Python Version Installed : ')
+print(sys.version,'\n')
+print('============================')
