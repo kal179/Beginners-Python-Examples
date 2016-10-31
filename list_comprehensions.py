a163fbf4dc0404e1b15ecb9e5acbe618c72dd84d
Kalpak Take <kalpaktake@gmail.com>
2016-10-31 10:28:53 +0530

    Create list_comprehensions.py

---
+++ list_comprehensions.py
@@ -0,0 +1,11 @@
+list_of_even_squares = [num ** 2 for num in range(0,101,2)]
+print(list_of_even_squares , "\n")
+
+list_of_odd_squares = [num ** 2 for num in range(1,102,2)]
+print(list_of_odd_squares , "\n")
+
+list_of_even_cubes = [num ** 3 for num in range(0,101,2)]
+print(list_of_even_cubes , "\n")
+
+list_of_odd_cubes = [num ** 3 for num in range(1,102,2)]
+print(list_of_odd_cubes , "\n")
