35fbde856af90a5786bf4d75a2257a1d59052bfb
Kalpak Take <kalpaktake@gmail.com>
2016-10-31 23:08:06 +0530

    Update table_maker.py

--- table_maker.py
+++ table_maker.py
@@ -1,4 +1,6 @@
 # Tables maker
 num = int(input("Number to make table : "))
-li_a = [num for num in range(0,num * 11,num)]
-print(li_a)
+li_a = [num for num in range(0 , num * 11 , num)]
+ 
+for digit in li_a:
+	print(digit)
