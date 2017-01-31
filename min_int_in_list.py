3498c4b4d09f727323ba245ef5f5749c21d3b845
Kalpak Take <kalpaktake@gmail.com>
2017-01-31 17:39:53 +0530

    Add files via upload

---
+++ min_int_in_list.py
@@ -0,0 +1,30 @@
+
+def  compare(li):
+	res = 0
+	for i in range(len(li) - 1):
+		a = li[i]
+		b = li[i + 1]
+		if (a < b):
+			if (i != 0): 
+				if (a < res):
+					res = a
+			else:
+				res = a 	
+		else:
+			if (i != 0):
+				if (b < res):
+					res = b
+			else:
+				res = b	
+	return res 
+	
+def convert():	
+	get_input = raw_input("Enter Space Seperated Numbers : ")
+	raw = get_input.split()
+	nums = []
+	for i in raw:
+		nums.append(float(i))
+	return nums
+
+if __name__ == "__main__":	
+	print compare(convert())		
