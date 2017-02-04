a1ad0ba06b893e4c11554d5a52a51beb834df894
Kalpak Take <kalpaktake@gmail.com>
2017-02-04 22:18:15 +0530

    Algorithm for sorting array in reverse order

---
+++ reverse_sort.py
@@ -0,0 +1,17 @@
+"""
+This sort is designed by me.
+First self designed Algorithm.
+Kalpak Take
+"""
+
+def reverse_sort(array):
+	for i in range(len(array) - 1):
+		for n in range(len(array) - 1):
+			a = array[n]
+			if (a < array[i]):
+				tem = array[i]
+				array[i] = a 
+				array[n] = tem 
+	return array			
+	
+print reverse_sort([123, 3455, 6577, 546, 345, 22, 56, 7])	
