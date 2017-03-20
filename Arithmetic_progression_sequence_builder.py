ddac9fc4cc7405257142f26d387723fdc7b2c0ce
Kalpak Take <kalpaktake@gmail.com>
2017-03-20 21:55:13 +0530

    Build arithmetic progression sequence on given args

---
+++ Arithmetic_progression_sequence_builder.py
@@ -0,0 +1,20 @@
+
+
+class Arithmetic_progression(object):
+
+	@staticmethod
+	def arithmetic_sequence_builder(a , diff = 0, limit = 0):
+		if (diff <= 0 or limit <= 0):
+			return "Error Invalid Arguments For Difference or Limit"
+
+		else:
+			if (limit == a):
+				return [a]
+			else:
+				seq = []
+				seq.append(a)
+				for i in range(a, limit, diff):
+					seq.append(i + (a - 1) * 2)	
+				return seq 
+			
+print(Arithmetic_progression.arithmetic_sequence_builder(2, 2, 2))
