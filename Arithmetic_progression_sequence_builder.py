42a7449709eb7241748a88bbdc4537f00198f546
Kalpak Take <kalpaktake@gmail.com>
2017-03-22 10:21:05 +0530

    Fixed sequence building

--- Arithmetic_progression_sequence_builder.py
+++ Arithmetic_progression_sequence_builder.py
@@ -12,13 +12,13 @@ class Arithmetic_progression(object):
 				return [a]
 			else:
 				seq = []
-				for i in range(0, limit, diff):
+				for i in range(a, limit, diff):
 					seq.append(i + (a - 1) * 2)	
 				return seq 
 # Testing			
 print(Arithmetic_progression.arithmetic_sequence_builder(2, 2, 100))
 print(Arithmetic_progression.arithmetic_sequence_builder(1, 10, 100))
-print(Arithmetic_progression.arithmetic_sequence_builder(11, 100, 10000))
+print(Arithmetic_progression.arithmetic_sequence_builder(1, 5, 10000))
 """
 Still Some Bugs are to be fixed algo is not perfect
 like sometimes it omits first value of a in seq
