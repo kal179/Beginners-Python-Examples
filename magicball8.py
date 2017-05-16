46d77a2b8f981bfa995714f2a800982691078c5a
Kalpak Take <kalpaktake@gmail.com>
2017-05-16 17:40:58 +0530

    reduced the size of code

--- magicball8.py
+++ magicball8.py
@@ -1,42 +1,22 @@
 # pre code
 import random
 
-# response list
-response = [ "It is certain" , "Outlook good" , "You may rely on it" , "Ask again later" ,
- "Concentrate and ask again" , "Reply hazy, try again" , "My reply is no" , "My sources say no" ]
-
 # func for telling answer and asking question
 def magic():
-    print(" ")
-    userQuesInput = raw_input("Ask me a question . Try me : ")
-    print(" ")
-    forAns = random.choice(response)
-    print(forAns)
+    userQuesInput = input("Ask me a question . Try me: ")
+    return random.choice([ "It is certain" , "Outlook good" , "You may rely on it" , "Ask again later" , "Concentrate and ask again" , "Reply hazy, try again" , "My reply is no" , "My sources say no" ])
 
 # main Sector
+print("Hello,")
 while True:
-    print("Hello,")
     # getting started
-    user  = raw_input("Start or End : ")
-    if user.strip() == "Start":
-        print(" ")
-        print(magic())
-    elif user.strip() == "End":
-        print(" ")
+    user  = input("Start or End: ").strip()
+    if user == "Start":
+        print(magic(), "\n")
+        continue
+    elif user == "End":
         print("Program ended...")
         break
     else :
-        print(" ")
         print("Invalid Input.Try again.")
         continue
-
-    # starting again
-    startAgain = raw_input("Start again or End : ")
-    if startAgain.strip() == "Start again":
-        print("Starting again...")
-        print(" ")
-        continue
-    elif startAgain.strip() == "End":
-        print("Program Ended...")
-        print(" ")
-        break
