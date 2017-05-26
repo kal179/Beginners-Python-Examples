cd996b264ae57d94d385f0839cb0a1784d31bfbb
Kalpak Take <kalpaktake@gmail.com>
2017-05-26 15:54:05 +0530

    more setup options

--- CompileFiles.py
+++ CompileFiles.py
@@ -5,14 +5,19 @@ from cx_Freeze import setup, Executable
 
 nameOfExec = input("Name for Executable: ")
 versionNumber = input("Version: ")
+auth = input("Name of Author: ")
+auth_email = input("Email of Author: ")
 descript = input("Description: ")
 filename = input("File to Compile(Add .py to file): ")
 
 # run this file in cmd "python CompileFiles.py build"
+# This setup in minimialistic
 
 setup(
     name = nameOfExec,
     version = versionNumber,
-    description = descript,
+    description = descript, 
+    author = auth,
+    author_email = auth_email,
     executables = [Executable(filename)]
     )
