6e074e2e9a3dad32fb1c315b1a5cfd61fa84d051
Kalpak Take <kalpaktake@gmail.com>
2017-07-09 07:48:13 +0530

    Update and rename collatz sequence to collatz_sequence.py

--- collatz_sequence.py
+++ collatz_sequence.py
@@ -3,45 +3,29 @@
 #operation performed is the number is even = number // 2 and if the number is odd = number * 3 + 1
 
 #example if the number is 5 , out put is 5 16 8 4 2 1
-
-
-
-#lets get started
-
-from sys import exit
+from sys import exit            # really no need to do that but as a precaution is good
 
 def collatz(num):                             # defining the function
-
-    if num % 2 == 0:
-
+    if (num % 2 == 0):
         res = num //2
 
-    elif num % 2 != 0:
-
+    elif (num % 2 != 0):
         res = num * 3 + 1
 
     else:
-
         print('Enter a valid integer')
 
-
-
-    while res == 1:                          #we need to exit the function after 1 is obtained
-
+    while (res == 1):                          #we need to exit the function after 1 is obtained
         print(res)
+        exit(0)
 
-        exit()
-
-    while res != 1:
-
+    while (res != 1):
         print(res)
-
         num = res
-
         collatz(num)                          #the function should be executing until 1 is obtained
 
 
+num = int(input('Enter an integer: '))           #asking for input and then calling the function also change input to raw_input in python 2x versions
 
-num = int(input('Enter an integer>> '))           #asking for input and then calling the function
-
+print("\nReaching one here....")
 collatz(num)
