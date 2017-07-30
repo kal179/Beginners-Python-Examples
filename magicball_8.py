36a8d1c876cddd562e4eaa39c43f5921f4d403ed
Kalpak Take <kalpaktake@gmail.com>
2017-07-30 22:46:37 +0530

    Update magicball_8.py

--- magicball_8.py
+++ magicball_8.py
@@ -1,14 +1,13 @@
 import random
 
-# func for telling answer and asking question
 def magic():
-    userQuesInput = input("Ask me a question . Try me: ")
+    input("Ask me a question . Try me: ")
     return random.choice([ "It is certain" , "Outlook good" , "You may rely on it" , "Ask again later" , "Concentrate and ask again" , "Reply hazy, try again" , "My reply is no" , "My sources say no" ])
 
 # main Sector
 print("Hello,")
 while True:
-    if input("Start or End: ").strip().lower() == "start":
+    if input("start or end: ").strip().lower() == "start":
         print(magic(), "\n")
         continue
     else:
