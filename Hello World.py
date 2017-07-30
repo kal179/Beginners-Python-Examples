2d0d8ce5a68aa8456774a49b0979e01ad3f3d251
Kalpak Take <kalpaktake@gmail.com>
2017-07-30 15:09:47 +0530

    Update and rename Hello World.py to hello_world.py

--- Hello World.py
+++
@@ -1,4 +0,0 @@
-# printing hello world is a tradition in beginners 
-# it is normally used to if check everything is okay
-
-print("Hello World!")
---
+++ hello_world.py
@@ -0,0 +1,8 @@
+# printing hello world is a tradition in beginners 
+# it is normally used to if check everything is okay
+import sys
+
+sys.stdout.write("Hello, ")
+sys.stdout.write("World!")
+sys.stdout.write("\n")
+print("Hello, World!")
