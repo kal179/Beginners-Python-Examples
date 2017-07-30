dd2ddb14b23db0ac783e8d6b4fa763e94d886d6a
Kalpak Take <kalpaktake@gmail.com>
2017-07-30 22:49:02 +0530

    Update and rename my name.py to my_name.py

--- my name.py
+++
@@ -1,4 +0,0 @@
-inside="abcdefghijklmnopqrs tuvwyxyz"
-print (inside[10]+inside[0]+inside[11]+inside[15]+inside[0]+inside[10]+ inside[19]+inside[20]\
-+inside[0]+inside[10]+inside[4])
-		
---
+++ my_name.py
@@ -0,0 +1,5 @@
+# Example of accessing elements in list
+
+inside = list("abcdefghijklmnopqrs tuvwyxyz")
+print (inside[10] + inside[0] + inside[11] + inside[15] + inside[0] + inside[10] + inside[19] + inside[20] + inside[0] + inside[10] + inside[4])
+		
