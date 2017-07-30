b108f36d76a72c2a96128f024209d929ef47d7cb
Kalpak Take <kalpaktake@gmail.com>
2017-07-30 22:35:40 +0530

    Update and rename quadrant.py to cartesian_plane_quadrant.py

---
+++ cartesian_plane_quadrant.py
@@ -0,0 +1,21 @@
+# quadrant determiner
+# I(+,+) II(-,+) III(-,-) IV(+,-)
+
+def determine_quadrant(x, y):
+	try:
+		if x > 0 and y > 0:
+			return 'I(+,+)' 
+		elif x < 0 and y > 0:
+			return 'II(-,+)'
+		elif x < 0 and y < 0 :
+			return 'III(-,-)'
+		elif x > 0 and y < 0 :
+			return 'IV(+,-)'
+		else :
+			return 'Invalid parameters were provided')
+	except TypeError:
+		return "X and Y co-ords must be integers and not X {}, Y{}".format(type(x), type(y))
+
+# Test
+result = determine_quadrant(float(input('X co-ordinate: ')), float(input('Y co-ordinate: ')))
+print("Quadrant is " + result)
