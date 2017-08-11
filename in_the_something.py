1a572d239f1ec3c2d203353ea366be20bd352463
Kalpak Take <kalpaktake@gmail.com>
2017-08-11 21:46:10 +0530

    Update in_the_something.py

--- in_the_something.py
+++ in_the_something.py
@@ -1,8 +1,13 @@
 import random
 
 # "The something in something" program
-noun_lib = ["cat","dog","lizard","bald","insane guy","CEO","monkey","teacher","ballerina","old man","nerd","lion","alien"]
+noun_lib = ["cat","dog","lizard","bald","insane guy","CEO","monkey","teacher","ballerina","old man","nerd","lion","alien","elephant"]
 place_lib = ["in Hungary","in the toilet","in a car","in a zoo","in a lions cave","in a park","in Norway","in Rio","on Mars","on a tree","on the roof of Burj-Khalifa"]
 
-frame = "The" + " " + random.choice(noun_lib) + " " + random.choice(place_lib) + "."
-print(frame)
+flag = True
+while flag:
+    inp = str(input("\nDo you want more? [Y/n] "))
+    if inp.strip().lower() == "y":
+        print("-> The" + " " + random.choice(noun_lib) + " " + random.choice(place_lib) + ".")
+    else:
+        flag = False
