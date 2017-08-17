0cb614f5f7c42d52f323f797f6ed55fec512de88
Kalpak Take <kalpaktake@gmail.com>
2017-08-17 22:17:50 +0530

    find y in ( b ** y = x) using logarithmic function (integers)

---
+++ logarithm_integer.py
@@ -0,0 +1,25 @@
+
+# Exhaustive numeration (iteration)
+# Simple implementation of logarithmic function
+# I love math!
+
+# log(b, x) <=> b ** y = x
+# So we have to find y!
+
+# Don't use it for decimal numbers
+def logarithm_integer(b, x):
+	if (b > 0 and b != 1) and x > 0:
+		for i in range(x):
+			if b ** i == x:
+				return i 
+		return -1
+	else:
+		return "Invalid input for logarithm"
+
+# Test
+print("log(6, 216) -> " + str(logarithm_integer(6, 216)))
+print("log(5, 625) -> " + str(logarithm_integer(5, 25)))
+print("log(4, 16) -> " + str(logarithm_integer(4, 16)))
+print("log(2, 16) -> " + str(logarithm_integer(2, 16)))
+print("log(3, 6) -> " + str(logarithm_integer(3, 6)))
+print("log(0, 16) -> " + str(logarithm_integer(0, 16)))
