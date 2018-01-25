6ff5ee5d105c050249be6af2e4387dfe591504ec
Kalpak Take <kalpaktake@gmail.com>
2018-01-25 12:02:13 +0530

    Update Number Guessing Game.py

--- Number Guessing Game.py
+++ Number Guessing Game.py
@@ -1,21 +1,37 @@
-"""this program asks person to guess a number if the number is correct the code breaks
-otherwise it runs continously as it is in infinite loop"""
-while(2==2):
-   print("Welcome to the Guessing Game")
-   print("Hint:number is in range 10")
-   start = raw_input("Enter Start or End:")
-   if start == "Start":
-       get = int(input("Guess the number:"))
-       if get == 5:
-           print("You Win")
-           break
-       elif get <= 4:
-             print("Number you entered is Too Low")
-       elif  get >= 6:
-              print("Number you enetred is Too High")
-   elif start == "End":
-       print("Game Ended")
-   else :
-       print("You Lose Sorry!")
-       print("Game Ended")
-           
+#!/usr/bin/python
+# -*- coding: <encoding name> -*-
+
+from random import randint as r_int 
+
+# generates a random number
+# in inclusive list from 0 to 10
+n = r_int(0, 10) 
+print("Hint: number is between 0 to 10 including 0 and 10\n")
+
+
+while True:
+	usr = int(raw_input("Guess a number: "))
+   
+	# Make sure user got the hint
+	if usr <= 10 and usr >= 0:
+
+		# if correct
+		if usr == n:
+			print("That's a correct guess!\nYou got it!\n")
+			break
+
+		# brings user closer to answer
+		elif usr < n:
+			print("Try a bigger num!")
+		else:
+			print("Try a smaller num!")
+
+	else:
+		print("Pls read the hint!")
+	
+	# Continue till guess does not match 'n'
+	# the random number
+	print("")
+	continue
+
+# Is an infinite loop if usr keeps guessing wrong
