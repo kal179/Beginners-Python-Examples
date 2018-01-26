496865ab0731e4ff3f19b30546dfb6defa45c3ab
Kalpak Take <kalpaktake@gmail.com>
2018-01-26 21:45:53 +0530

    Add reverse sort feature

--- algorithms/analysis/sort.py
+++ algorithms/analysis/sort.py
@@ -2,14 +2,18 @@
 # -*- coding: utf-8 -*-
 
 # Bubble Sort
-# Good sorting algorithm for 
+# Ideal sorting algorithm for 
 # small data/array
 
 # temporary parameter tells
 # function to sort a copy of 
 # orignal array, and not the array itself
 
-def sort_(arr, temporary = False):
+# reverse parameter tells func
+# to sort array in reverse/decending
+# order
+
+def sort_(arr, temporary = False, reverse = False):
 	
 	# Making copy of array if temporary is true
 	if temporary:
@@ -17,7 +21,8 @@ def sort_(arr, temporary = False):
 	else:
 		ar = arr
 	
-	# To blend every element in correct position
+	# To blend every element
+	# in correct position
 	# length of total array is required
 	length = len(ar)
 	
@@ -29,12 +34,25 @@ def sort_(arr, temporary = False):
 		# through un-sorted elements
 		for i in range(0, length - 1):
 
-			# Swapping greater elements to right
-			# and smaller to left
-			if ar[i] > ar[i+1]:
-				tmp = ar[i]
-				ar[i] = ar[i + 1]
-				ar[i + 1] = tmp
+			if reverse:
+
+				# Swapping greater elements to left
+				# and smaller to right
+				# decending order
+				if ar[i] < ar[i+1]:
+					tmp = ar[i]
+					ar[i] = ar[i + 1]
+					ar[i + 1] = tmp
+			else:
+
+				# Swapping greater elements to right
+				# and smaller to left
+				# accending order
+				if ar[i] > ar[i+1]:
+					tmp = ar[i]
+					ar[i] = ar[i + 1]
+					ar[i + 1] = tmp
+
 
 		# making sure loop breaks
 		length = length - 1 
@@ -52,16 +70,17 @@ def sort_(arr, temporary = False):
 
 
 # Testing
-tests = [[9, 8, 7, 6, 5, 4, 3, 2, 1, 0], [1312, 1110, 98, 76, 54, 32, 10], ] # Add your test cases 
+tests = [[7, 8, 9, 6, 4, 5, 3, 2, 1, 15], [1, 90, 1110, 1312, 1110, 98, 76, 54, 32, 10], ] # Add your test cases 
+
 for test in tests:
-	c = sort_(test, True) 
-	if c == sorted(test):
+	accend, decend = sort_(test, True), sort_(test, True, True)
+	if accend == sorted(test) and decend == sorted(test, reverse = True):
 		print("Orignal: {}".format(test))
-		print("Sorted: {}".format(c))
-		print("It worked!\n")
+		print("Sorted: {}".format(accend))
+		print("Sorted(reverse): {}\n".format(decend))
 	else:
 		print("Something went wrong!\n")
 
 
 # Seems our bubble sort works
-# however for small data or array!
+# however for small data/array!
