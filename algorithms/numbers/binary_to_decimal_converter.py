790d8b78d31c3fdfa55590baec6cb10c0a0b1c6d
Kalpak Take <kalpaktake@gmail.com>
2018-01-28 14:19:22 +0530

    Update and rename binary_to_decimal_converter.py to algorithms/numbers/binary_to_decimal_converter.py

---
+++ algorithms/numbers/binary_to_decimal_converter.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+import sys
+
+# Binary to decimal conversion
+# See explaination: https://i.imgur.com/heAT0PB.gif   , 
+# https://www.electronics-tutorials.ws/binary/bin_2.html
+
+# Pesudo code:
+# Iterate through binary num
+# if 0 then pass
+# else result = result + bit(1) * 2 ** index of bit(1)
+
+def binary_to_decimal_conv(binary_string):
+	res = 0
+	binary_l = list(binary_string)
+	for bit_i in range(len(binary_l)):
+		res += int(binary_l[bit_i]) * (2 ** bit_i) 
+	return res 
+
+# Test
+# Testing interface
+i = 0
+while True:
+	if raw_input("\n[{}] Exit(press e) or Continue(press c): ".format(i)).strip().lower() == "c":
+		print("Decimal form: " + str(binary_to_decimal_conv(raw_input("\nBinary?: "))))
+	else:
+		print("\nHope you enjoyed!")
+		sys.exit()
+	i += 1
