b0cb0bc260790bd468acde85304ff3860b76c26f
K R Anand <anandkryelwal@gmail.com>
2018-02-15 15:38:37 +0530

    Added with ... as option

--- writingFiles.py
+++ writingFiles.py
@@ -6,3 +6,14 @@ fileToWrite = open(fileName, 'w') # 'w' writes to the file
 textToWrite = str(input('Text to write : '))
 fileToWrite.write(textToWrite) # writing file
 fileToWrite.close() # closing the file
+
+# In the above example you have invoke closure on the file descriptor, fileToWrite 
+# If some error occurs the file descriptors are not closed and this may cause problems
+# One can use context manager with ... as to avoid this (>python 2.5)
+# close function is not required as it automatically closes the file once the context is over
+# Same as a try ... except ... finally block
+
+textToWrite = str(input('Text to write : '))
+fileName = str(input('File name : ')) 
+with open(filename,'w') as fileToWrite:
+  fileToWrite.write(textToWrite) 
