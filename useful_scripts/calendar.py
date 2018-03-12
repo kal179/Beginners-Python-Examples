5d08059df9790f2626995d1e9c47a1a18f1bb358
Kalpak Take <kalpaktake@gmail.com>
2018-03-12 16:30:22 +0530

    made code more elegant

--- useful_scripts/calendar.py
+++ useful_scripts/calendar.py
@@ -1,17 +1,17 @@
 # calender viewer
-import calendar
 import sys
+import calendar
 
 # function to view calendar
-def viewCalender(yy, mm):
-	# Void Function
-	print('\n\n Calendar > \n' + calendar.month(yy , mm) + "\n")
+viewCalender = lambda yy, mm: print("\n\n Calendar > \n %s\n" % calendar.month(yy, mm))
 
 # UI
 while True:
 	if str(input("[+] Start [Y/n] ?  ")).strip().lower() == "y":
-		viewCalender(int(input('\nYear : ')), int(input('Month : ')))
+		try:
+			viewCalender(int(input("\nYear: ")), int(input("Month: ")))
+		except IndexError:
+			print("  -> Try Again! With valid numbers!\n")
 	else:
 		print("\nSee Ya Soon!")
 		sys.exit(0)
-		
