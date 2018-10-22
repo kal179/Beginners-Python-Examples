63e2fc798a45227a2a5ab6dd7633a18b41abfb45
hakerja <joseph.haker@gmail.com>
2018-10-22 17:47:29 -0400

    Added function to check whether number is prime. Made user input simpler for user. Fixed a typo.

--- primeNumbers.py
+++ primeNumbers.py
@@ -1,25 +1,30 @@
 # Prime number Determiner
-# replace input() with raw_input() in Python version 2.7 input() works with version 3 
+# replace input() with raw_input() in Python version 2.7 input() works with version 3
+
+def isPrime(a):
+	# If num is prime, returns the given number.
+	# If num is not prime, returns the first number it is divisible by.
+	if a > 2:
+		for x in list(range(2,int(a/2)+1)):
+			if a % x == 0:
+				return x
+				break
+		return a
+	else:
+		return a
 
 while True:
 	startOrEnd = str(input('Start or End : '))
-	if startOrEnd == 'Start':
-		toCheckNum = int(input('Number to Check : '))
-		if toCheckNum > 1:
-			for x in range(2, toCheckNum):
-				if toCheckNum % x == 0:
-					print(str(toCheckNum) + ' is divisible by ' + str(x))
-					print(str(toCheckNum) + ' is not a Prime number')
-					break
-				elif toCheckNum % x > 0:
-					print(str(toCheckNum) + ' is a prime number')
-					break
-			continue				
-		else :
-			print(str(toCheckNum) + ' is not a prime number')
-			continue					
-	else : 
-		print('Progarm Ended...')
-		break		
-
-
+	if startOrEnd.upper().strip() == 'START':
+		toCheckNum = int(input('Number to check: '))
+		a = isPrime(toCheckNum)
+		if a == toCheckNum:
+			print(str(toCheckNum) + ' is a prime number.')
+			continue
+		else:
+			print(str(toCheckNum) + ' is divisible by ' + str(a) + '.')
+			print(str(toCheckNum) + ' is not a prime number.')
+			continue
+	else :
+		print('Program Ended...')
+		break
