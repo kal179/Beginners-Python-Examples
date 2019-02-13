0e6ca0f3388626c33a1d4f59266af441c75b1422
crackCodeLogn <vivektheintel@gmail.com>
2019-02-13 21:52:45 +0530

    Adding comments for understanding

--- primeNumbers.py
+++ primeNumbers.py
@@ -7,6 +7,20 @@ NEGATIVE_MESSAGE = " is not a prime number"
 
 
 def is_number_prime(number):
+    """
+    Function which checks whether the number is a prime number or not
+    :param number: integer - to be checked for prime-ness
+    :return: boolean - true if prime, else false
+    """
+
+    """
+    This is the main logic behind reducing the numbers to check for as factors
+        if N = a * b; where a<=b and a,b C (1, N)
+        then, a * b >= a*a;
+        which leads to => a*a <= N
+                       => a <= sqrt(N)
+    Hence checking only till the square root of N 
+    """
     upper_lim = Math.floor(Math.sqrt(number)) + 1
     is_prime = True if number != 1 else False
 
@@ -14,6 +28,7 @@ def is_number_prime(number):
         if number % i == 0:
             is_prime = False
             break
+            # The moment there is a divisor of 'number', break the iteration, as the number is not prime
 
     return is_prime
 
