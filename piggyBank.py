50aa4448f56b29c2540c409a93f19e00fb756ed8
Naresh Prabhu <43432358+nareshp24@users.noreply.github.com>
2019-02-19 22:09:01 -0500

    Update piggyBank.py
    Made the program object oriented.

--- piggyBank.py
+++ piggyBank.py
@@ -1,57 +1,72 @@
-# piggy bank
-# pre code
-money = 0
+class PiggyBank():
+    ''' Piggy Bank class '''
 
-# function to add money to current amount
-def addMoney():
-    print(" ")
-    userAdd = float(raw_input("Add money : "))
-    print(" ")
-    money = money + userAdd
-    print("After adding current Money you have is " + str(money) + " rupees")
+    #class variable
+    currency = 'USD'
 
-# function to withdraw money from current amount
-def withdrawMoney():
-    print(" ")
-    userWithdraw = float(raw_input("Add money : "))
-    print(" ")
-    money = money + userWithdraw
-    print("After adding current Money you have is " + str(money) + " rupees")
+    def __init__(self, name=None, balance=0.0):
+        '''Initializes the instance '''
+        self.name = name
+        self.balance = balance
 
-# function to display current amount
-def currentMoney():
-    print(" ")
-    current = "Current money you have is " + str(money) + " rupees"
+    def deposit(self, money=0.0):
+        '''Add money to your account'''
+        self.balance += money
+            
+    def display(self):
+        '''Display the current balance'''
+        return self.balance
 
-# main code
-print(" ")
-print("--------------------Start-------------------")
-while True:
-    print(" ")
-    user = raw_input("Start or End : ")
-    if user.strip() == "Start":
-        controlPiggy = raw_input("Add Withdraw or Check : ")
-        if controlPiggy.strip() == "Add":
-            print(addMoney())
-            continue
-        elif controlPiggy.strip() == "Withdraw":
-            print(withdrawMoney())
-            continue
-        elif controlPiggy.strip() == "Check":
-            print(currentMoney())
-            continue
-        else :
-            print(" ")
-            print("Invalid Input.Try again")
-            continue
+    def withdraw(self, money=0.0):
+        '''withdraw money from your account'''
+        self.balance -= money
 
-    elif user.strip() == "End" :
-        print(" ")
-        print("------------Program Ended-----------")
-        print(" ")
-        break
+    def showName(self):
+        '''show user name'''
+        return self.name
 
-    else :
-        print(" ")
-        print("Invalid Input. Try again")
-        continue
+
+if __name__== "__main__":
+    
+    #Take input from user
+    name = input('Enter the name of the user: ')
+    initBal = float(input('Enter starting balance: '))
+
+    account = PiggyBank(name, initBal) #create an instance
+
+    flag = 1
+    while(flag):
+        print('\n------Press the numbers accordingly to perform the operation-----')
+        print('1. Deposit money\n2. Withdraw money\n3. Show current balance')
+        print('4. Show account name\n5. Quit')
+
+        cmd = int(input())
+
+        if cmd == 1:
+            money = float(input('Enter the amount to deposit: '))
+            account.deposit(money)
+            print('Current balance is {} {}'.format(account.display(), account.currency))
+
+        elif cmd == 2:
+            money = float(input('Enter the amount to withdraw: '))
+            account.withdraw(money)
+            print('Current balance is {} {}'.format(account.display(), account.currency))
+
+        elif cmd == 3:
+            print('Current balance is {} {}'.format(account.display(), account.currency))
+
+        elif cmd == 4:
+            print('Account name: {}'.format(account.name))
+
+        elif cmd == 5:
+            while(flag):
+                check = input('Are you sure you want to exit? [y / n]: ')
+                
+                if check == 'y' or check == 'Y':
+                    print('exiting the program. See you again!')
+                    flag = 0
+                else:
+                    break
+            
+            
+        
