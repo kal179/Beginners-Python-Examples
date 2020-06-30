f994997b4414d75710ae1a9f7ec03750c0958dd8
Kamalesh Pathy VA <asokankamalesh@gmail.com>
2020-06-30 20:50:22 +0530

    Modified the code from python 2.x to 3.x and added some error handling

--- find_cube_root.py
+++ find_cube_root.py
@@ -5,14 +5,21 @@
 # Kinda brute forcing
 
 def find_cube_root(x):
-    if type(x) == str:
-        return "Expected an integer! Cannot find cube root of an string!"
-    for i in range(0, x):
-        if i ** 3 == x:
-            return i 
-    return "{} is not a perfect cube".format(x)
-    
-# Test 
+	try:
+		x = int(x)
+		for i in range(0, x):
+			if i ** 3 == x:
+				return i
+		return f"{x} is not a perfect cube"
+
+	except ValueError:
+		return "Expected an integer! Cannot find cube root of an string!"
+
+
+# Test
 x = 27
 result = find_cube_root(x)
-print("Cube root of {} is {}".format(x, result))
+if type(result) == int:
+	print("Cube root of {} is {}".format(x, result))
+else:
+	print(result)
