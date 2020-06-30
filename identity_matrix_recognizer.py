e38277a47becdc747627a1ca9f4d2f89ae976dea
Kamalesh Pathy VA <asokankamalesh@gmail.com>
2020-06-30 20:08:21 +0530

    Modified the syntax from python2.x to 3.x and reduced some lines of code but does the same

--- identity_matrix_recognizer.py
+++ identity_matrix_recognizer.py
@@ -1,13 +1,13 @@
 # from cs 101 course of udacity.com (problem set solved solution)
 
-# Given a list of lists representing a n * n matrix as input, 
-# define a  procedure that returns True if the input is an identity matrix 
+# Given a list of lists representing a n * n matrix as input,
+# define a  procedure that returns True if the input is an identity matrix
 # and False otherwise.
 
-# An IDENTITY matrix is a square matrix in which all the elements 
-# on the principal/main diagonal are 1 and all the elements outside 
-# the principal diagonal are 0. 
-# (A square matrix is a matrix in which the number of rows 
+# An IDENTITY matrix is a square matrix in which all the elements
+# on the principal/main diagonal are 1 and all the elements outside
+# the principal diagonal are 0.
+# (A square matrix is a matrix in which the number of rows
 # is equal to the number of columns)
 
 def is_identity_matrix(matrix):
@@ -19,10 +19,8 @@ def is_identity_matrix(matrix):
             last_pos += 1
         else:
             return False
-    if total_elems == len(matrix[0]) * len(matrix[0]):
-        return True
-    else:
-        return False
+    return total_elems == len(matrix[0]) * len(matrix[0])
+
 
 
 # Test Cases:
@@ -31,47 +29,47 @@ matrix1 = [[1,0,0,0],
            [0,1,0,0],
            [0,0,1,0],
            [0,0,0,1]]
-print is_identity_matrix(matrix1)
+print(is_identity_matrix(matrix1))
 #>>>True
 
 matrix2 = [[1,0,0],
            [0,1,0],
            [0,0,0]]
 
-print is_identity_matrix(matrix2)
+print(is_identity_matrix(matrix2))
 #>>>False
 
 matrix3 = [[2,0,0],
            [0,2,0],
            [0,0,2]]
 
-print is_identity_matrix(matrix3)
+print(is_identity_matrix(matrix3))
 #>>>False
 
 matrix4 = [[1,0,0,0],
            [0,1,1,0],
            [0,0,0,1]]
 
-print is_identity_matrix(matrix4)
+print(is_identity_matrix(matrix4))
 #>>>False
 
 matrix5 = [[1,0,0,0,0,0,0,0,0]]
 
-print is_identity_matrix(matrix5)
+print(is_identity_matrix(matrix5))
 #>>>False
 
-matrix6 = [[1,0,0,0],  
-           [0,1,0,1],  
-           [0,0,1,0],  
+matrix6 = [[1,0,0,0],
+           [0,1,0,1],
+           [0,0,1,0],
            [0,0,0,1]]
 
-print is_identity_matrix(matrix6)
+print(is_identity_matrix(matrix6))
 #>>>False
 
 matrix7 = [[1, -1, 1],
            [0, 1, 0],
            [0, 0, 1]]
-print is_identity_matrix(matrix7)
-#>>>False           
+print(is_identity_matrix(matrix7))
+#>>>False
 
-           
+
