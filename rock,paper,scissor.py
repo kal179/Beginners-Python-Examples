907ebce494e76e874f33caf67a6c837fef44364c
hsteg420 <72036132+hsteg420@users.noreply.github.com>
2020-10-04 09:56:35 +0530

    Updated code for better result

--- rock,paper,scissor.py
+++ rock,paper,scissor.py
@@ -1,49 +1,23 @@
 import random
-while 2 == 2:
-    print('--------------------------------------------------------------')
-    get_again = raw_input('Enter rock paper or scissor :  ')
-    make_use = random.choice(['rock','paper','scissor'])
-    print('--------------------------------------------------------------')
-    print(make_use)
-    print('--------------------------------------------------------------')
-    if get_again == 'rock' and make_use == 'paper':
-        print('computer wins! You "LOSE" ')
-        print('--------------------------------------------------------------')
-        continue
-    elif get_again == 'rock' and make_use == 'scissor':
-        print('computer loses You win!')
-        print('--------------------------------------------------------------')
-        break
-    elif get_again == 'rock' and make_use == 'rock':
-        print('It\'s a tie ! try again !')
-        print('--------------------------------------------------------------')
-        continue
-    elif get_again == 'paper' and make_use == 'paper':
-        print('It\'s a tie ! try again !')
-        print('--------------------------------------------------------------')
-        continue
-    elif get_again == 'scissor' and make_use == 'scissor':
-        print('It\'s a tie ! try again !')
-        print('--------------------------------------------------------------')
-        continue
-    elif get_again == 'paper' and make_use == 'scissor':
-        print('computer wins! You "LOSE" ')
-        print('--------------------------------------------------------------')
-        continue
-    elif get_again == 'scissor' and make_use == 'paper':
-        print('computer loses You win!')
-        print('--------------------------------------------------------------')
-        break
-    elif get_again == 'paper' and make_use == 'rock':
-        print('computer wins! You "LOSE" ')
-        print('--------------------------------------------------------------')
-        continue
-    elif get_again == 'scissor' and make_use == 'rock':
-        print('computer wins You "LOSE" ')
-        print('--------------------------------------------------------------')
-        continue
-    else :
-        print('Invalid input: Try again')
-        continue
+print("Welcome to Rock, Paper, Scissors Game")
+uchoice = input("Scissors, Rock, Paper ?")
+choice = ['Scissors', 'Rock', 'Paper']
+mchoice = random.choice(choice)
+print(mchoice)
 
-    
+if mchoice == uchoice:
+    print("Tie")
+elif mchoice == choice[0] and uchoice == 'Rock':
+    print("You win")
+elif mchoice == choice[2] and uchoice == 'Rock':
+    print("You loose")
+elif mchoice == choice[0] and uchoice == 'Paper':
+    print("You loose")
+elif mchoice == choice[1] and uchoice == 'Paper':
+    print("You win")
+elif mchoice == choice[1] and uchoice == 'Scissors':
+    print("You loose")
+elif mchoice == choice[2] and uchoice == 'Scissors':
+    print("You win")
+else:
+    print("Please enter valid choice!")
