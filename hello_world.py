274d63cb333788e294598eeb93b7d0b4fb9badc5
Rohitm619 <rohitm.official404@gmail.com>
2020-10-15 18:35:15 +0530

    Added a comment in hello_word.py

--- hello_world.py
+++ hello_world.py
@@ -1,8 +1,11 @@
 # printing hello world is a tradition in beginners 
 # it is normally used to if check everything is okay
+#I am commenting for testing purpose and u dont mind
+
 import sys
 
 sys.stdout.write("Hello, ")
 sys.stdout.write("World!")
 sys.stdout.write("\n")
 print("Hello, World!")
+
