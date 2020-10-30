f2b1820fc4f4d623de21c6d18a7847a96ce6b214
Kalpak Take <20800689+AsciiKay@users.noreply.github.com>
2020-10-30 22:37:15 +0530

    Update and rename Slot Machine.py to slot_machine.py

---
+++ shell_games/slot_machine.py
@@ -0,0 +1,16 @@
+import random
+
+while True:
+  # set of options
+  options = ["Skull","Diamond","Heart","Star","Blood"]
+  # random outcome 
+  for i in range(0,3):
+    print(random.choice(options))
+    
+  # play again? 
+  if int(input("Press 1 if you Would You Like To Play Again? ")) == 1:
+    print("")
+    continue
+  else:
+    print("You earned.")
+    break
