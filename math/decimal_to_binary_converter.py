1346afc2456ae4a41c56eee73667714e16710c61
Kalpak Take <20800689+zeta-punk-x1@users.noreply.github.com>
2023-01-17 18:30:28 +0530

    Create decimal_to_binary_converter.py
    I came up with this algorithm to convert decimal(natural numbers) to binary completely from scratch and therefore it might not be the best most efficient implementation.
    Optimizations are welcome.

---
+++ math/decimal_to_binary_converter.py
@@ -0,0 +1,16 @@
+
+def dec_to_bin(n):
+    quo = n 
+    binary = 0
+    while (quo>0):
+        tmp = quo
+        pows = 0
+        while (quo > 1):
+            quo = quo // 2
+            pows = pows + 1 
+        quo = tmp-pow(2,pows)
+        binary = binary+pow(10,pows)
+    return binary
+
+for i in range(0,101):
+    print(dec_to_bin(i))
