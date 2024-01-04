7a35c9024a58b8e9bbcbfc6eeabd43206246657e
Karthik110505 <bvvkarthik1155@gmail.com>
2024-01-04 22:56:42 +0530

    Improving the code to work for numbers of any length

--- armstrong_number.py
+++ armstrong_number.py
@@ -1,4 +1,4 @@
-# Python program to check if the number is an Armstrong number with the index of 3 or not
+''# Python program to check if the number is an Armstrong number with the index of 3 or not
 # for input try numbers 153, 370, 371, 407
 
 # take input from the user
@@ -6,12 +6,14 @@ num = int(input("Enter a number: "))
 
 # initialize sum
 sum = 0
+# finding the length of num
+n = len(str(num))
 
 # find the sum of the cube of each digit
 temp = num
 while temp > 0:
    digit = temp % 10
-   sum += digit ** 3
+   sum += digit ** n # power of n
    temp //= 10
 
 # display the result
@@ -23,3 +25,4 @@ else:
 
 
 # Originally contribution by denz647
+''
